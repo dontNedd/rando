30980f65319b046f5c19330919d54b51baeefe52
Sick the Cat <129430216+sickthecat@users.noreply.github.com>
2023-06-09 20:55:52 -0400

    Update swinstall.sh
    mew. pretty.

--- swinstall.sh
+++ swinstall.sh
@@ -1,25 +1,79 @@
 #!/bin/bash
-#Mar 30 2023
-#nedd
-#install steam and wine with updates
+# Mar 30 2023
+# nedd
+# Install steam and wine with updates - Modded by _SiCk https://afflicted.sh/
 
 
-sudo apt update
+# Define colors for formatting
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
 
-sudo apt-get upgrade
+# Function to display menu
+display_menu() {
+    clear
+    echo -e "${GREEN}=== Steam and Wine Installer ===${NC}\n"
+    echo "Please choose an option:"
+    echo "1. Update package lists"
+    echo "2. Upgrade installed packages"
+    echo "3. Enable the multiverse repository"
+    echo "4. Install Steam"
+    echo "5. Enable 32-bit architecture"
+    echo "6. Add Wine repository"
+    echo "7. Install Wine"
+    echo "8. Quit"
+    echo
+}
 
-sudo add-apt-repository multiverse
+# Function to perform actions based on user choice
+perform_action() {
+    case $1 in
+        1)
+            echo -e "\n${YELLOW}Updating package lists...${NC}"
+            sudo apt update
+            ;;
+        2)
+            echo -e "\n${YELLOW}Upgrading installed packages...${NC}"
+            sudo apt-get upgrade
+            ;;
+        3)
+            echo -e "\n${YELLOW}Enabling the multiverse repository...${NC}"
+            sudo add-apt-repository multiverse
+            ;;
+        4)
+            echo -e "\n${YELLOW}Installing Steam...${NC}"
+            sudo apt install steam
+            ;;
+        5)
+            echo -e "\n${YELLOW}Enabling 32-bit architecture...${NC}"
+            sudo dpkg --add-architecture i386
+            ;;
+        6)
+            echo -e "\n${YELLOW}Adding Wine repository...${NC}"
+            sudo add-apt-repository ppa:ubuntu-wine/ppa
+            ;;
+        7)
+            echo -e "\n${YELLOW}Installing Wine...${NC}"
+            sudo apt-get update
+            sudo apt-get install wine1.8
+            ;;
+        8)
+            echo -e "${YELLOW}Quitting...${NC}"
+            exit 0
+            ;;
+        *)
+            echo -e "${YELLOW}Invalid option.${NC}"
+            ;;
+    esac
 
-sudo apt update
+    # Pause before returning to the menu
+    read -n 1 -s -r -p "Press any key to continue..."
+}
 
-sudo apt install steam
-
-sudo dpkg --add-architecture i386
-
-sudo add-apt-repository ppa:ubuntu-wine/ppa
-
-sudo apt-get update
-
-sudo apt-get install wine1.8
-
-echo "Steam and Wine are now installed"
+# Main loop
+while true
+do
+    display_menu
+    read -p "Enter your choice: " choice
+    perform_action $choice
+done
